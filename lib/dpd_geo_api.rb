5c1444373f9e95ea1ee12ab9746f126a142976eb
Kuba Malina <kuba@mixit.cz>
2024-06-11 23:56:10 +0200

    Creates main classes of workflow.

--- lib/dpd_geo_api.rb
+++ lib/dpd_geo_api.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "dpd_geo_api/version"
+require_relative "dpd_geo_api/client"
+require_relative "dpd_geo_api/request"
+require_relative "dpd_geo_api/request_maker"
+require_relative "dpd_geo_api/response"
+require_relative "dpd_geo_api/response_validator"
 
 module DpdGeoApi
   class Error < StandardError; end
---
+++ lib/dpd_geo_api/client.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module DpdGeoApi
+  class Client
+    attr_reader :api_secret, :api_url, :test_api
+
+    def initialize(api_secret, api_url, test_api: false)
+      @api_secret = api_secret
+      @api_url = api_url.last == "/" ? api_url[0..-2] : api_url
+      @test_api = test_api
+    end
+
+    def me
+      # TODO: implement
+    end
+  end
+end
