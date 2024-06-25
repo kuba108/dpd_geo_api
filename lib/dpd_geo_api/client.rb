f58c041a8467bd51a949d70292c3d99b5df400f0
Kuba Malina <kuba@mixit.cz>
2024-06-25 18:33:13 +0200

    Adds basic tests.

--- lib/dpd_geo_api/client.rb
+++ lib/dpd_geo_api/client.rb
@@ -6,7 +6,7 @@ module DpdGeoApi
 
     def initialize(api_secret, api_url = "https://geoapi.dpd.cz/v1", test_api = false)
       @api_secret = api_secret
-      @api_url = api_url.last == "/" ? api_url[0..-2] : api_url
+      @api_url = api_url[-1] == "/" ? api_url[0..-2] : api_url
       @test_api = test_api
     end
 
--- spec/dpd_geo_api_spec.rb
+++ spec/dpd_geo_api_spec.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe DpdGeoApi do
+  let(:api_secret) { 'a2447d590f3d7705a7ef4353c6ff39fd' }
+  let(:api_url) { 'https://geoapi.dpd.cz/v1' }
+  let(:client) { DpdGeoApi::Client.new(api_secret, api_url) }
+
   it "has a version number" do
     expect(DpdGeoApi::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#initialize' do
+    it 'initializes with correct api_secret and api_url' do
+      expect(client.api_secret).to eq(api_secret)
+      expect(client.api_url).to eq(api_url)
+    end
+  end
+
+  describe '#me' do
+    before do
+      allow_any_instance_of(DpdGeoApi::RequestMaker).to receive(:get_request).and_return('response')
+    end
+
+    it 'calls the API method "/me"' do
+      expect(client.me).to eq('response')
+    end
   end
 end
